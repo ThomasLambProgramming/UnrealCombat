214c8148e65f294559314a683ccf00c25484458d
Thomas Lamb <thomaslambprogramming@gmail.com>
2023-10-15 14:31:02 +1100

    Fixed default map and rider compiler issue

--- Source/Alchemist/Alchemist.Build.cs
+++ Source/Alchemist/Alchemist.Build.cs
@@ -8,6 +8,6 @@ public class Alchemist : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "EnhancedInput", "GameplayTasks", "AIModule", "NavigationSystem"});
 	}
 }
