fabcc16aced024af51a0ce517fe63cb533c58f63
Thomas Lamb <thomaslambprogramming@gmail.com>
2024-12-29 03:31:10 +1100

    Removed project since I wanted to rename it and it was easier to start again

--- Source/ArkhamAttempt/ArkhamAttempt.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "ArkhamAttempt.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ArkhamAttempt, "ArkhamAttempt" );
- 
